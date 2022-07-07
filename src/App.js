9f6333abbb148bf088e4ce4f14aa5c1ca20549ac
henri Perillat <henri.perillat@eemi.com>
2022-07-07 16:47:43 +0200

    testing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery/dist/jquery.min.js'
-import 'bootstrap/dist/js/bootstrap.min.js'
+
 
 
 
